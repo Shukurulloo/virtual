2de215ebb9acd4fe0a7aebec28cce7b1a6ef5e19
mac <mac@macui-MacBookPro.local>
2023-07-06 20:28:45 +0900

    tahrir

--- src/components/pages/container1.jsx
+++ src/components/pages/container1.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { Container, Photo, Photo1, Restangle, Restangle2,  Restangle3, Restangle4, Restangle5, Restangle6, Restangle7, Restangle8, Restangle9, Restangle11, Restangle12, Restangle13} from './style'
-import { Navbar, Container01, Web, Home, Wrap, Wrap2, Smile, Smile2, Web2, Container2, Web3, Time} from "./style"
+import { Container,  Photo1, Restangle, Restangle2,} from './style'
+import { Navbar, Container01, Web, Home, Wrap, Wrap2, Smile, Smile2, Web2, Container2, Web3, Time, Container3, Web4, Web40,Container4, Container5,Container6} from "./style"
 import { Button0} from "./style"
-import photo from './assets/Logo.jpg'
 import img001 from './assets/Logo.jpg'
 import img02 from './assets/Photo.jpg'
 import img03 from './assets/Group1.jpg'
@@ -32,11 +31,11 @@ const Container1 = () => {
         <Navbar>
 
             <Web left>
-              <Photo src={photo}/>
+              <img src={img001} alt='' style={{marginLeft:"140px"}}/>
             <h1>Credischool</h1>
             </Web>
                   <Web mid>
-                    <Home hom>Home</Home>
+                    <Home hom >Home</Home>
                     <Home>Campus</Home>
                     <Home>Education</Home>
                     <Home>Alumni</Home>
@@ -56,7 +55,7 @@ const Container1 = () => {
             <Button0>Learn more</Button0>
             </Wrap>
 
-                 <Wrap2>
+                 <Wrap2 style={{marginTop:"-50px"}}>
             <Smile smile01>
               <img src={img03} alt=''/>
               <Smile2>
@@ -74,59 +73,58 @@ const Container1 = () => {
               <Container2>
                 <Web3 secure>
                   <Restangle src={restangle1} />
-                  <web30>24/7 Support</web30>
-                  <web31>Always online to help you</web31>
+                  <h2>24/7 Support</h2>
+                  <p>Always online to help you</p>
                 </Web3>
                  <Web3 secure>
-                   <Restangle2 src={restangle2} />
-                   <web30>Secure Payments</web30>
-                   <web31>Pay your semesters fast & secure</web31>
+                   <Restangle src={restangle2} />
+                   <h2>Secure Payments</h2>
+                   <p>Pay your semesters fast & secure</p>
                 </Web3>
                  <Web3 support>
-                 <Restangle3 src={restangle3} />
-                  <web30>Online Lectures</web30>
-                  <web31>Study in real time, review later</web31>
+                 <Restangle src={restangle3} />
+                  <h2>Online Lectures</h2>
+                  <p>Study in real time, review later</p>
                 </Web3>
               </Container2>
 
-              <container3>
-                  <explore>Explore the departments</explore>
-                  <web4>
-                  <Restangle4 src={restangle4} />
-                    <web40 computer>
-                      <web401>Computer Science</web401>
-                      <web4011>Our computer science department has multiple learning paths, from Mobile and Web development, to Machine Learning and Data Science.</web4011>
+              <Container3>
+              <h2 className='cont3'>Explore the departments</h2>
+                <Web4>
+                  <Restangle2 src={restangle4} />
+                    <Web40 computer>
+                      <h1>Computer Science</h1>
+                      <p>Our computer science department has multiple learning paths, from Mobile and Web development, to Machine Learning and Data Science.</p>
+                      <Button0>Learn more</Button0>
+                    </Web40>
+                  </Web4>
+                  <Web4>
+                  <Web40 style={{marginLeft:"120px"}}>
+                      <h1>Chemical Engineering</h1>
+                      <p students>All of the students that are enrolled in the  Chemical Engineering program receive a  Chemist’s Kit to practice while learning from  home.</p>
                       <Button0>Learn more</Button0>
                       
-                    </web40>
-                  </web4>
-                  <web4>
-                  <web40 >
-                      <web401>Chemical Engineering</web401>
-                      <web4011 students>All of the students that are enrolled in the  Chemical Engineering program receive a  Chemist’s Kit to practice while learning from  home.</web4011>
+                    </Web40>
+                    <Restangle2 src={restangle5} style={{marginLeft:"120px"}} />
+                    </Web4>
+                  <Web4>
+                  <Restangle2 src={restangle6} />
+                  <Web40 computer>
+                      <h1>Business & Economics</h1>
+                      <p>The future CEOs and Entrepreneurs have an opportunity to study the business climate and analyize real world case studies of business  success stories.</p>
                       <Button0>Learn more</Button0>
                       
-                    </web40>
-                    <Restangle5 src={restangle5} />
-                    </web4>
-                  <web4>
-                  <Restangle6 src={restangle6} />
-                  <web40 computer>
-                      <web401>Business & Economics</web401>
-                      <web4011>The future CEOs and Entrepreneurs have an opportunity to study the business climate and analyize real world case studies of business  success stories.</web4011>
-                      <Button0>Learn more</Button0>
-                      
-                    </web40>
-                  </web4>
-              </container3>
-              <container4>
-                <web501>More than 50.000 students
-            has enrolled this year</web501>
-                <Restangle7 src={restangle7} />
+                    </Web40>
+                  </Web4>
+              </Container3>
+              <Container4>
+             <h2>More than 50.000 students
+            has enrolled this year</h2>
+                <img src={restangle7} alt='' />
 
                <Time> 
                 <Smile>
-              <Restangle8 src={restangle8}/>
+              <img src={restangle8} alt=''/>
               <Smile2>
               <h4>Students </h4>
               <h1>150.000+</h1>
@@ -134,7 +132,7 @@ const Container1 = () => {
             </Smile >
 
             <Smile zones>
-            <Restangle9 src={restangle9}/>
+            <img src={restangle9} alt='' style={{marginLeft: '40px'}}/>
               <Smile2>
               <h4 time>Time Zones </h4>
               <h1>10+</h1>
@@ -143,52 +141,50 @@ const Container1 = () => {
             </Smile>
             </Time>
           
-              </container4>
-              <container5>
-
-            <interested>
-              <interesh1> Interested? Let’s get you started</interesh1>
+              </Container4>
+              <Container5>
+              <div className='wrapper'>
+              <h1> Interested? Let’s get you started</h1>
             <Button0 register>Register</Button0>
-            </interested>
-            
-            </container5>
+            </div>
+            </Container5>
+       
+            <Container6>
+            <div className='HeaderNav' style={{alignItems:"flex-end"}}>
+            <Web left2 >
+              <img src={img001} alt=''/>
+              <h1 style={{marginRight:"40px"}}>Credischool</h1>
+              </Web>
+              <p>Copyright © 2020 Credischool University. All Right Reserved</p>
+              </div>
 
-            <container6>
-              <container601>
-                <credisshool>
-                <img src={img001} alt=''/>
-                <h1>Credischool</h1>
-                </credisshool>
-                <p>Copyright © 2020 Credischool University. All Right Reserved</p>
-              </container601>
+              <div className='HeaderNav' style={{flex:"3"}} >
+              <div className='contnent'>
+              <Home hom>Home</Home>
+              <Home>Campus</Home>
+              <Home>Education</Home>
+              <Home>Alumni</Home>
+              <Home>About</Home>
+              </div>
 
-              <container602>
-              <educartion>
-              <homee>Home</homee>
-                <h4>Campus</h4>
-                <h4>Education</h4>
-                <h4>Alumni</h4>
-                <h4>About</h4>
-                </educartion>
+              <div className='contnent'>
+              <Home >Terms</Home>
+              <Home>Privacy</Home>
+              <Home>GDPR</Home>
+              </div >
+              </div>
 
-                <educartion>
-                <h4>Terms</h4>
-                <h4>Privacy</h4>
-                <h4>GDPR</h4>
-                </educartion>
-              </container602>
-
-              <container603>
-                <logo>
-                <Restangle11 src={restangle11} />
-                <Restangle12 src={restangle12} />
-                <Restangle13 src={restangle13} />
-                </logo>
-                <designed>
-              <p>Designed with <red>♥</red> by <red>nikolovlazar.com</red></p>
-              </designed>
-              </container603>
-            </container6>
+              <div className='HeaderNav' right>
+              <div className='logo'>
+              <img src={restangle11} alt=''  />
+              <img src={restangle12} alt='' style={{marginLeft:"20px"}}/>
+              <img src={restangle13} alt='' style={{marginLeft:"20px"}}/>
+              </div>
+              <div className='text' >
+              <p>Designed with <span className='red'>♥</span > by <span className='red'>nikolovlazar.com</span ></p>
+              </div >
+              </div>
+            </Container6>
 
     </Container>
 
@@ -196,3 +192,4 @@ const Container1 = () => {
 }
 
 export default Container1
+
--- src/components/pages/style.js
+++ src/components/pages/style.js
@@ -7,240 +7,6 @@ export const Container = styled.div`
 display: flex;
 flex-direction: column;
 
-
-container6{
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    flex-direction: row;
-    height: 250px;
-}
-container601{
-    display: flex;
-    flex: 2;
-    justify-content: center;
-    align-items: flex-end;
-    flex-direction:column;
-
-   
-}
-container601 p{
-    margin-top: 20px;
-    width: 241px;
-    height: 46px;
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 500;
-    font-size: 12px;
-    line-height: 23px;
-    color: #030F4B;
-
-}
-
-
-container602{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction:column;
-    flex: 3;
-    
-}
-educartion{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: row;
-}
-educartion h4{
-width: 71px;
-    height: 22px;
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 15px;
-    line-height: 22px;
-    color: #808396;
-    margin-left:20px;
-
-}
-
-container603{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction:column;
-    flex: 2;
-    
-}
-credisshool{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: row;
-   
-}
-credisshool h1{
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 800;
-    font-size: 24px;
-    line-height: 29px;
-    color: #030F4B;
-    margin-left: 20px;
-    margin-right: 34px;
-}
-designed p{
-  margin-left:-80px;
-    font-size: 11px;
-font-family: Inter;
-font-style: normal;
-font-weight: 500;
-line-height: 23px;
-}
-red{
-    color:red;
-}
-
-
-
-container5{
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-    flex-direction: row;
-    padding-left:-20px;
-    height: 700px;
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-image: url(${props2});
-}
-interested{
-    margin-top: -100px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: row;
-    width: 1200px;
-    height: 180px;
-    border-radius: 6px;
-    box-shadow: 0px 4px 6px 1px rgba(0, 0, 0, 0.05), 0px 10px 15px 4px rgba(0, 0, 0, 0.10);
-    background: #FCFCFF;
-}
-interesh1{
-    width: 513px;
-    height: 39px;
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 800;
-    font-size: 32px;
-    line-height: 39px;
-    color: #030F4B;
-    margin-right:40px;
-}
-container4{
-    display: flex;
-                justify-content: center;
-                align-items: center;
-                flex-direction: column;
-                width:100%;
-                margin-top: 150px;
-                margin-bottom: 230px;
-}
-web501{
-    width: 873px;
-    height: 154px;    
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 800;
-    font-size: 64px;
-    line-height: 77px;
-    text-align: center;
-    color: #030F4B; 
-}
-            container3{
-                  display: flex;
-                justify-content: center;
-                align-items: center;
-                flex-direction: column;
-                width:100%;
-}
-explore{
-    width: 785px;
-    height: 77px;
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 800;
-    font-size: 64px;
-    line-height: 77px;
-    color: #030F4B;
-    margin-top:30px;
-}
-web4{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: row;
-    margin-top: 90px;
-}
-web40{
-    display: flex;
-    justify-content: center;
-    /* align-items: start; */
-    flex-direction: column;
-    /* margin-left:${props => props.computer ? "180px" : " 0px"}; */
-   /* margin-right: 40px; */
-}
-web401{
-    height: 58px;   
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 800;
-    font-size: 48px;
-    line-height: 58px;
-    color: #030F4B;
-}
-web4011{
-    margin-top: 40px;
-    height: 114px;
-    width: 526px;
-    /* width:${props => props.students ? "700px" : "542px"}; */
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 500;
-    font-size: 24px;
-    line-height: 38px;
-    color: #4A4E63;
-    margin-bottom:70px;
-}
-          
-            web30:hover{
-                font-style: oblique;
-                cursor: pointer;
-                background-color: #c0f3df;
-                cursor: pointer;
-                text-decoration: underline;
-}
-            web30{
-                height: 29px;
-                font-family: 'Inter';
-                font-style: normal;
-                font-weight: 700;
-                font-size: 22px;
-                line-height: 29px;
-                color: #030F4B;
-                margin-top: 20px;
-            }
-            web31{
-                margin-top: 10px;
-                height: 22px;
-                font-family: 'Inter';
-                font-style: normal;
-                font-weight: 500;
-                font-size: 16px;
-                line-height: 22px;
-                color: #52596E;
-            }
 `
 export const Navbar = styled.div`
 margin-top:60px;
@@ -251,70 +17,22 @@ margin-top:60px;
     flex-direction: row;
     height: 122px;
     width: 80%;
-    /* border: 1px solid black; */
-  
 `
 export const Photo1 = styled.img`
 width: 815px;
 height: 31px;
+margin-top:-7px
 `
-
 export const Restangle = styled.img`
-
     border-radius: 4px;
     width: 280px;
-    /* height:188px; */
-    box-shadow: 0px 10px 35px -3px rgba(51, 93, 255, 0.35), 0px 4px 6px rgba(51, 93, 255, 0.25);
+    box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
 `
 export const Restangle2 = styled.img`
-     width: 280px;
-    /* height:188px; */
-    border-radius: 4px;
-    box-shadow: 0px 10px 35px -3px rgba(51, 93, 255, 0.35), 0px 4px 6px rgba(51, 93, 255, 0.25);
-`
-export const Restangle3 = styled.img`
- width: 280px;
-    /* height:188px; */
-    border-radius: 4px;
-    box-shadow: 0px 10px 35px -3px rgba(51, 93, 255, 0.35), 0px 4px 6px rgba(51, 93, 255, 0.25);
-`
-export const Restangle4 = styled.img`
 margin-right: 120px;
 width: 450px;
  box-shadow: 0px 10px 35px -3px rgba(51, 93, 255, 0.35), 0px 4px 6px rgba(51, 93, 255, 0.25);
     border-radius: 13px;
-`
-export const Restangle5 = styled.img`
-margin-left: 120px;
-width: 450px;
- box-shadow: 0px 10px 35px -3px rgba(51, 93, 255, 0.35), 0px 4px 6px rgba(51, 93, 255, 0.25);
-    border-radius: 13px;
-`
-export const Restangle6 = styled.img`
-margin-right: 120px;
-width: 450px;
- box-shadow: 0px 10px 35px -3px rgba(51, 93, 255, 0.35), 0px 4px 6px rgba(51, 93, 255, 0.25);
-    border-radius: 13px;
-`
-export const Restangle7 = styled.img`
-
-`
-export const Restangle8 = styled.img`
-
-`
-export const Restangle9 = styled.img`
-margin-left: 40px;
-`
-export const Restangle11 = styled.img`
-
-`
-export const Restangle12 = styled.img`
-margin-left: 20px;
-
-`
-export const Restangle13 = styled.img`
-margin-left: 20px;
-
 `
 export const Button0 = styled.button`
  box-shadow: 0px 10px 35px -3px rgba(51, 93, 255, 0.35), 0px 4px 6px rgba(51, 93, 255, 0.25);
@@ -324,22 +42,15 @@ export const Button0 = styled.button`
     align-items: center;
     padding: 15px 40px;
     width: ${props => props.register || props.register1  ? "140px" : "164px;"};
-    /* height: 52px; */
     background-color: ${props => props.register || props.register1 ? "#FD4455" : "#335DFF"};
-    /* background-color: #FD4455; */
     box-shadow:${props => props.register || props.register1 ? "0px 10px 35px -3px rgba(253, 68, 85, 0.35), 0px 4px 6px rgba(253, 68, 85, 0.25)" : "0px 20px 65px -9px rgba(68, 102, 253, 0.35), 0px 7px 9px rgba(87, 68, 253, 0.25);"};
     border-radius: 8px;
-    font-family: 'Inter';
-    font-style: normal;
     font-weight: 700;
     font-size: 15px;
     line-height: 22px;
     color: #FCFCFF;
     border: none;
     margin-right: 40px;
-   
-
-
 `
 export const Container01 = styled.div`
     display: flex;
@@ -362,15 +73,9 @@ align-items: center;
 flex: ${props=>props.left || props.right ? "1" : "1" };
 justify-content: ${props=>props.right ? "right" : "left"};
 h4{
-    font-family: 'Inter';
-    font-style: normal;
     font-weight: 600;
     font-size: 15px;
-    /* line-height: 22px; */
     color: #030F4B;
-    /* padding-right: 100px;
-    margin-right: -130px;
-    margin-left: 140px; */
     margin-right: 40px;
 }
 h1{
@@ -378,8 +83,6 @@ h1{
     padding-top: 3px;
     width: 145px;
     height: 29px;
-    font-family: 'Inter';
-    font-style: normal;
     font-weight: 900;
     font-size: 24px;
     line-height: normal;
@@ -388,30 +91,20 @@ h1{
 `
 
 export const Home = styled.div`
-    font-family: 'Inter';
-    font-style: normal;
     font-weight:${props => props.hom ? "600;" : "500;"};
     font-size: 15px;
     line-height: 22px;
-    margin-left:${props => props.hom ? "0px" : "40px"};
+    margin-left:${props => props.hom ? "70px" : "40px"};
     color: ${props => props.hom ? "#030F4B;" : "#808396;"};
-
 `
-export const Photo = styled.img`
-margin-left:140px;
-`
-
 export const Wrap = styled.div`
    flex: 1;
     display: flex;
     flex-direction: column;
-    /* margin-top:100px; */
     margin-left: -430px;
 h1{
     margin-top: 140px;
    
-   font-family: 'Inter';
-   font-style: normal;
    font-weight: 900;
    font-size: 72px;
    line-height: 97px;
@@ -421,11 +114,8 @@ h1{
 }
 p{
     margin-top: 50px;
-    /* margin-left: -700px; */
     width: 528px;
     height: 156px;
-    font-family: 'Inter';
-    font-style: normal;
     font-weight: 500;
     font-size: 21px;
     line-height: 158%;
@@ -440,20 +130,17 @@ export const Wrap2 = styled.div`
  align-items: flex-end;
 flex-direction: column;
 margin-bottom: 20px;
-/* border: 1px solid black; */
 margin-bottom:  50px;
 `
 export const Time = styled.div`
     display: flex;
 `
 export const Smile = styled.div`
-/* border: 1px solid black; */
 display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: row;
     height: 168px;
-    /* width: 378px; */
     width:${props => props.zones ? "378px" : "425px"};
     background:${props => {return props.zones ? "#FD4455" : "#335DFF"}};
     box-shadow: ${props => props.zones ? "0px 10px 35px -3px rgba(253, 68, 85, 0.35), 0px 4px 6px rgba(253, 68, 85, 0.25)" : "box-shadow: 0px 10px 35px -3px rgba(51, 93, 255, 0.35), 0px 4px 6px rgba(51, 93, 255, 0.25)"};
@@ -471,8 +158,6 @@ h1{
 
     width: 259px;
     height: 48px;
-    font-family: 'Inter';
-    font-style: normal;
     font-weight: 800;
     font-size: 38px;
     line-height: 48px;
@@ -482,14 +167,11 @@ h1{
 h4{
     width: 237px;
     height: 29px;
-    font-family: 'Inter';
-    font-style: normal;
     font-weight: 700;
     font-size: 21px;
     line-height: 29px;
-    color:${props => props.time ? "#91A7FF" : "#FCEDEF"};#91A7FF
-    /* color: #91A7FF; */
-    margin-top:30px;;
+    color:${props => props.time ? "#91A7FF;" : "#FCEDEF;"};
+    margin-top:30px;
 }
 
 `
@@ -498,24 +180,179 @@ export const Web2 = styled.div`
    height: 37px; 
     margin-left:857px;
     margin-top: -21px;
-    /* border: 1px solid black; */
 `
 export const Container2 = styled.div`
                 height: 460px;
-                /* border: 1px solid black; */
                 width:100%; 
                 display: flex;
                 justify-content: space-evenly;
                 align-items: center;
                 flex-direction: row;
-                /* height: 580px; */
 `
 export const Web3 = styled.div`
-                margin-top:80px;
+                margin-top:150px;
                 display: flex;
                 justify-content: center;
                 align-items: center;
                 flex-direction: column;
                 margin-right:${props => props.secure || props.support ? "-200px" : "-100px"};
                 margin-left:-100px;
+h2{
+                height: 29px;
+        
+        
+                font-weight: 700;
+                font-size: 22px;
+                line-height: 29px;
+                color: #030F4B;
+                margin-top: 10px;
+}
+p{
+                margin-top: -10px;
+                height: 22px;
+                font-weight: 500;
+                font-size: 16px;
+                line-height: 22px;
+                color: #52596E;
+}
+`
+export const Container3 = styled.div`
+                  display: flex;
+                justify-content: center;
+                align-items: center;
+                flex-direction: column;
+                width:100%;
+h2{
+    width: 785px;
+    height: 77px;
+    font-weight: 800;
+    font-size: 64px;
+    line-height: 77px;
+    color: #030F4B;
+    margin-top:30px;
+}
+`
+export const Web4 = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    margin-top: 90px;
+
+`
+export const Web40 = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+h1{
+    height: 58px;   
+    font-weight: 800;
+    font-size: 48px;
+    line-height: 58px;
+    color: #030F4B;
+}
+p{
+    margin-top: 40px;
+    height: 114px;
+    width: 526px;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 38px;
+    color: #4A4E63;
+    margin-bottom:70px;
+} 
+`
+export const Container4 = styled.div`
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                flex-direction: column;
+                width:100%;
+                margin-top: 150px;
+                margin-bottom: 230px;
+h2{
+    width: 873px;
+    height: 154px;    
+    font-weight: 800;
+    font-size: 64px;
+    line-height: 77px;
+    text-align: center;
+    color: #030F4B; 
+
+}
+`
+export const Container5= styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    flex-direction: row;
+    padding-left:-20px;
+    height: 700px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-image: url(${props2});
+.wrapper{
+    margin-top: -100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    width: 1200px;
+    height: 180px;
+    border-radius: 6px;
+    box-shadow: 0px 4px 6px 1px rgba(0, 0, 0, 0.05), 0px 10px 15px 4px rgba(0, 0, 0, 0.10);
+    background: #FCFCFF;
+}
+h1{
+    width: 513px;
+    height: 39px;
+    font-weight: 800;
+    font-size: 32px;
+    line-height: 39px;
+    color: #030F4B;
+    margin-right:40px;
+}
+`
+export const Container6 = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-direction: row;
+    height: 250px;
+.HeaderNav{
+    display: flex;
+    flex:${props => props.left || props.right ? "2" : "2"};
+    justify-content: center;
+    align-items: center;
+    flex-direction:column;
+}
+p{
+    margin-top: 20px;
+    width: 241px;
+    height: 46px;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 23px;
+    color: #030F4B;
+
+}
+.contnent{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    margin-bottom: 30px;
+}
+.logo{
+    margin-right:40px;
+}
+.text p{
+  margin-left:-80px;
+    font-size: 11px;
+font-weight: 500;
+line-height: 23px;
+}
+.red{
+    color:red;
+}
 `
